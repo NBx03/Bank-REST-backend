633767202998be1e9c9e0a7ee4c50de696ec1fe4
NBx03 <hopm123dustier@gmail.com>
2025-10-22 00:52:54 +0700

    Добавил аннотацию EntityGraph к UserRepository, чтобы роли автоматически извлекались во время аутентификации

--- src/main/java/com/example/bankcards/repository/UserRepository.java
+++ src/main/java/com/example/bankcards/repository/UserRepository.java
@@ -3,12 +3,19 @@ package com.example.bankcards.repository;
 import com.example.bankcards.entity.User;
 import com.example.bankcards.entity.enums.UserStatus;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Override
+    @EntityGraph(attributePaths = "roles")
+    Optional<User> findById(Long id);
+
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByEmail(String email);
 
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByUsername(String username);
 
     boolean existsByEmail(String email);
